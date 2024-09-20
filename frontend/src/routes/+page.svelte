c960d76280b716f1bf0a1386ca514aa33a4dc2a1
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-09-20 22:55:15 +0545

    add: theme & customization

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,59 +1,38 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { goto } from '$app/navigation';
+	import Button from '@/components/ui/button/button.svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<main class="justify-content-center flex flex-col items-center">
+	<div class="relative overflow-hidden py-24 lg:py-32">
+		<div aria-hidden="true" class="absolute -top-96 start-1/2 flex -translate-x-1/2 transform">
+			<div
+				class="h-[44rem] w-[25rem] -translate-x-[10rem] rotate-[-60deg] transform bg-gradient-to-r from-background/50 to-background blur-3xl"
+			/>
+			<div
+				class="h-[50rem] w-[90rem] origin-top-left -translate-x-[15rem] -rotate-12 rounded-full bg-gradient-to-tl from-primary-foreground via-primary-foreground to-background blur-3xl"
+			/>
+		</div>
+		<div class="relative z-10">
+			<div class="container py-10 lg:py-16">
+				<div class="mx-auto max-w-2xl text-center">
+					<p class="">URLify</p>
+					<div class="mt-5 max-w-2xl">
+						<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+							Cleaner, Better and Concise Links
+						</h1>
+					</div>
+					<div class="mt-5 max-w-3xl">
+						<p class="text-xl text-muted-foreground">
+							Make your business links cleaner, better and concise with URLify
+						</p>
+					</div>
+					<div class="mt-8 flex justify-center gap-3">
+						<Button size={'lg'} on:click={() => {goto("/register")}}>Create an Account</Button>
+						<Button size={'lg'} variant={'outline'} on:click={() => {goto("/dashboard")}}>Dashboard</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import { Button } from '@/components/ui/button';
 	import {
 		Card,
@@ -10,47 +11,58 @@
 	} from '@/components/ui/card';
 	import { Input } from '@/components/ui/input';
 	import { Label } from '@/components/ui/label';
+	import { redirect } from '@sveltejs/kit';
 
-    let username : string = '';
-    let name : string = '';
-    let password : string = '';
+	let username: string = '';
+	let name: string = '';
+	let password: string = '';
 
-    async function registerUser(){
-        console.log("Submitting Data: ", name, username,password);
-    }
-
+	async function registerUser() {
+		console.log('Submitting Data: ', name, username, password);
+	}
 </script>
 
 <section>
-    <form on:submit|preventDefault={registerUser}>
-	<div class="flex min-h-screen items-center justify-center bg-background">
-		<Card class="w-full max-w-sm">
-			<CardHeader>
-				<CardTitle class="text-center text-2xl font-bold">Register</CardTitle>
-				<CardDescription class="text-center"
-					>Register to use URLify</CardDescription
-				>
-			</CardHeader>
-			<CardContent class="space-y-4">
-				<div class="space-y-2">
-					<Label for="name">Name</Label>
-					<Input id="name" bind:value={name} placeholder="Enter your name" required />
-				</div>
-				<div class="space-y-2">
-					<Label for="username">Username</Label>
-					<Input id="username" bind:value={username} placeholder="Enter your username"  required />
-				</div>
-				<div class="space-y-2">
-					<Label for="password">Password</Label>
-					<Input id="password" type="password" bind:value={password} placeholder="Enter your password" required />
-				</div>
-			</CardContent>
-			<CardFooter class="flex flex-column">
-				<Button class="w-full" type="submit" >Sign In</Button><br>
-                <!-- <Button class="w-full" variant="secondary" >  Login Instead </Button> -->
-			</CardFooter>
-
-		</Card>
-	</div>
-    </form >
+	<form on:submit|preventDefault={registerUser} use:enhance method="post">
+		<div class="flex min-h-screen items-center justify-center bg-background">
+			<Card class="w-full max-w-sm">
+				<CardHeader>
+					<CardTitle class="text-center text-2xl font-bold">Register</CardTitle>
+					<CardDescription class="text-center">Register to use URLify</CardDescription>
+				</CardHeader>
+				<CardContent class="space-y-4">
+					<div class="space-y-2">
+						<Label for="name">Name</Label>
+						<Input id="name" bind:value={name} placeholder="Enter your name" required />
+					</div>
+					<div class="space-y-2">
+						<Label for="username">Username</Label>
+						<Input id="username" bind:value={username} placeholder="Enter your username" required />
+					</div>
+					<div class="space-y-2">
+						<Label for="password">Password</Label>
+						<Input
+							id="password"
+							type="password"
+							bind:value={password}
+							placeholder="Enter your password"
+							required
+						/>
+					</div>
+				</CardContent>
+				<CardFooter class="flex flex-col">
+					<Button class="w-full" type="submit">Register</Button><br />
+					<Button
+						class="w-full"
+						variant="outline"
+						on:click={() => {
+							window.location.href = '/login';
+						}}
+					>
+						Login Instead
+					</Button>
+				</CardFooter>
+			</Card>
+		</div>
+	</form>
 </section>
