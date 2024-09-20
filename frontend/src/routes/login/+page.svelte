318be729449b4c444a82f22cd135eeca15fe6881
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-09-20 22:13:31 +0545

    setup shadcn + tailwind

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+	import {
+		Card,
+		CardContent,
+		CardDescription,
+		CardFooter,
+		CardHeader,
+		CardTitle
+	} from '$lib/components/ui/card';
+</script>
+
+<section>
+	<div class="flex min-h-screen items-center justify-center bg-background">
+		<Card class="w-full max-w-sm">
+			<CardHeader>
+				<CardTitle class="text-center text-2xl font-bold">Login</CardTitle>
+				<CardDescription class="text-center"
+					>Enter your credentials to access your URLify account</CardDescription
+				>
+			</CardHeader>
+			<CardContent class="space-y-4">
+				<div class="space-y-2">
+					<Label for="username">Username</Label>
+					<Input id="username" placeholder="Enter your username" required />
+				</div>
+				<div class="space-y-2">
+					<Label for="password">Password</Label>
+					<Input id="password" type="password" placeholder="Enter your password" required />
+				</div>
+			</CardContent>
+			<CardFooter>
+				<Button class="w-full">Sign In</Button>
+			</CardFooter>
+		</Card>
+	</div>
+</section>
---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { Button } from '@/components/ui/button';
+	import {
+		Card,
+		CardContent,
+		CardDescription,
+		CardFooter,
+		CardHeader,
+		CardTitle
+	} from '@/components/ui/card';
+	import { Input } from '@/components/ui/input';
+	import { Label } from '@/components/ui/label';
+
+    let username : string = '';
+    let name : string = '';
+    let password : string = '';
+
+    async function registerUser(){
+        console.log("Submitting Data: ", name, username,password);
+    }
+
+</script>
+
+<section>
+    <form on:submit|preventDefault={registerUser}>
+	<div class="flex min-h-screen items-center justify-center bg-background">
+		<Card class="w-full max-w-sm">
+			<CardHeader>
+				<CardTitle class="text-center text-2xl font-bold">Register</CardTitle>
+				<CardDescription class="text-center"
+					>Register to use URLify</CardDescription
+				>
+			</CardHeader>
+			<CardContent class="space-y-4">
+				<div class="space-y-2">
+					<Label for="name">Name</Label>
+					<Input id="name" bind:value={name} placeholder="Enter your name" required />
+				</div>
+				<div class="space-y-2">
+					<Label for="username">Username</Label>
+					<Input id="username" bind:value={username} placeholder="Enter your username"  required />
+				</div>
+				<div class="space-y-2">
+					<Label for="password">Password</Label>
+					<Input id="password" type="password" bind:value={password} placeholder="Enter your password" required />
+				</div>
+			</CardContent>
+			<CardFooter class="flex flex-column">
+				<Button class="w-full" type="submit" >Sign In</Button><br>
+                <!-- <Button class="w-full" variant="secondary" >  Login Instead </Button> -->
+			</CardFooter>
+
+		</Card>
+	</div>
+    </form >
+</section>
