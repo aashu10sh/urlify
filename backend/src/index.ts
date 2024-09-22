2c6cf8104bc5aaaf2097e000fcdfbafa992bc0ef
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-09-22 19:18:17 +0545

    add: cors fix backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,18 @@ import authRouter from "./modules/auth/router/authRouter";
 import { logger } from "hono/logger";
 import linkRouter from "./modules/link/router/linkRouter";
 require("dotenv").config();
+import { cors } from 'hono/cors'
+
 
 const port = 3000;
 
 const app = new Hono().basePath("/api/v1");
+app.use('*',cors(
+  {
+    origin:['http://localhost:5173', 'http://localhost:4173'],
+    allowMethods:['POST','OPTIONS','GET','PUT','DELETE','PATCH','HEAD']
+  }
+));
 app.use(logger());
 
 app.get("/", (c) => {
